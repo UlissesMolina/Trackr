38017581992ea30db021d9c3cd0f6146de5ea422
Ulisses Molina-Becerra <umolina2005@gmail.com>
2026-02-21 13:58:35 -0600

    fixes

--- server/src/index.ts
+++ server/src/index.ts
@@ -61,6 +61,6 @@ app.use("/api/resume", resumeRouter);
 
 app.use(errorHandler);
 
-app.listen(PORT, () => {
-  console.log(`Server running on http://localhost:${PORT}`);
+app.listen(Number(PORT), "0.0.0.0", () => {
+  console.log(`Server running on 0.0.0.0:${PORT}`);
 });
